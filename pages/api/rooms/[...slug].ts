b076372f662b8fc5cdfcf4063a955beb43b44c4f
Jose J. Cruz <jjzcru@gmail.com>
2021-10-11 19:37:19 -0400

    u

--- pages/api/rooms/[...slug].ts
+++ pages/api/rooms/[...slug].ts
@@ -1,11 +1,16 @@
-import moment from 'moment-timezone'
-import { addReservation, cancelReservation, getRoomById, updateReservation } from '../../../data/db/RoomDB';
+import moment from 'moment-timezone';
+import {
+	addReservation,
+	cancelReservation,
+	getRoomById,
+	updateReservation,
+} from '../../../data/db/RoomDB';
 import { getTokenData } from '../../../data/services/auth';
 import { cors, runMiddleware } from '../../../middleware/cors';
 
 export default async function handler(req, res) {
 	let tokenData;
-  try {
+	try {
 		await runMiddleware(req, res, cors);
 		tokenData = await getTokenData(req);
 	} catch (e) {
@@ -26,25 +31,25 @@ export default async function handler(req, res) {
 		}
 	}
 
-  if (req.method === 'POST') {
-    try {
+	if (req.method === 'POST') {
+		try {
 			if (slug.length === 2) {
 				if (slug[1] === 'book') {
 					await reqAddReservation(req, res, tokenData);
 					return;
-				} else if(slug[1] === 'update') {
-          await reqUpdateReservation(req, res, tokenData);
+				} else if (slug[1] === 'update') {
+					await reqUpdateReservation(req, res, tokenData);
 					return;
-        } else if(slug[1] === 'cancel') {
-          await reqCancelReservation(req, res, tokenData);
+				} else if (slug[1] === 'cancel') {
+					await reqCancelReservation(req, res, tokenData);
 					return;
-        }
+				}
 			}
 		} catch (e) {
 			res.status(400).json({ message: e.message, status: 'error' });
 			return;
 		}
-  }
+	}
 
 	res.status(400).json({
 		message: 'Not found',
@@ -57,21 +62,26 @@ async function reqGetRoomById(req, res) {
 	const roomId = slug[0];
 	const room = await getRoomById(roomId);
 	let tz = req.headers['x-time-zone'] || 'America/New_York';
-        let now;
-        try {
-            now = moment(new Date()).tz(tz);
-        } catch(e) {
-            tz = 'America/New_York';
-            now = moment(new Date()).tz('America/New_York');
-        }
-
+	let now;
+	try {
+		now = moment(new Date()).tz(tz);
+	} catch (e) {
+		tz = 'America/New_York';
+		now = moment(new Date()).tz('America/New_York');
+	}
 	if (room) {
 		console.log(`Available times:`);
 		console.log(room.availableTimes);
 		room.availableTimes = room.availableTimes.filter((time) => {
 			const timeMoment = moment(time, 'HH:mm');
-			console.log(`This moment '${timeMoment.format('hh:mm A')}' is greather after ${now.format('hh:mm A')}: ${timeMoment.isAfter(now)}`);
-			return timeMoment.isAfter(now);
+			console.log(
+				`This moment '${timeMoment.format(
+					'hh:mm A'
+				)}' is greather after ${now.format(
+					'hh:mm A'
+				)}: ${timeMoment.isAfter(now)}`
+			);
+			return timeMoment.isAfter(moment(now.format('HH:mm'), 'HH:mm'));
 		});
 		res.status(200).json({ room, status: 'success' });
 		return;
@@ -86,17 +96,16 @@ async function reqAddReservation(req, res, tokenData) {
 	const { body } = req;
 	const { time } = body;
 	let tz = req.headers['x-time-zone'] || 'America/New_York';
-        let now;
-        try {
-            now = moment(new Date()).tz(tz);
-        } catch(e) {
-            tz = 'America/New_York';
-            now = moment(new Date()).tz('America/New_York');
-        }
-		const timeMoment = moment(time, 'HH:mm').tz(tz);
-		if(timeMoment.isAfter(now)) {
-			
-		}
+	let now;
+	try {
+		now = moment(new Date()).tz(tz);
+	} catch (e) {
+		tz = 'America/New_York';
+		now = moment(new Date()).tz('America/New_York');
+	}
+	const timeMoment = moment(time, 'HH:mm').tz(tz);
+	if (timeMoment.isAfter(now)) {
+	}
 	const success = await addReservation(employeeId, time, roomId);
 	if (success) {
 		res.status(200).json({ status: 'success' });
@@ -106,7 +115,7 @@ async function reqAddReservation(req, res, tokenData) {
 }
 
 async function reqUpdateReservation(req, res, tokenData) {
-  const { slug } = req.query;
+	const { slug } = req.query;
 	const roomId = slug[0];
 	const employeeId = tokenData.id;
 	const { body } = req;
@@ -119,9 +128,8 @@ async function reqUpdateReservation(req, res, tokenData) {
 	res.status(404).json({ message: 'Room not found', status: 'error' });
 }
 
-
 async function reqCancelReservation(req, res, tokenData) {
-  const { slug } = req.query;
+	const { slug } = req.query;
 	const roomId = slug[0];
 	const employeeId = tokenData.id;
 	const success = await cancelReservation(employeeId, roomId);
@@ -131,4 +139,3 @@ async function reqCancelReservation(req, res, tokenData) {
 	}
 	res.status(404).json({ message: 'Room not found', status: 'error' });
 }
-
