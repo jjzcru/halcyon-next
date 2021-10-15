6f5648ad6d612c3001e996f79b14701c66e86908
Jose J. Cruz <jjzcru@gmail.com>
2021-10-14 20:09:47 -0400

    feat: :sparkles: Add support for sound in db

---
+++ data/__tests__/SoundDB.test.ts
@@ -0,0 +1,24 @@
+import { getSounds, getSound, getSoundByType } from '../db/SoundDB'
+
+test('Should get the list of all the sounds', async () => {
+    const sounds = await getSounds();
+    expect(typeof sounds).toBe('object');
+    expect(sounds.length).toBeGreaterThan(0);
+});
+
+test('Should get the list of all the sounds that are type "sound"', async () => {
+    const type = 'sound';
+    const sounds = await getSoundByType(type);
+    expect(typeof sounds).toBe('object');
+    expect(sounds.length).toBeGreaterThan(0);
+    for(const sound of sounds) {
+        expect(sound.type).toBe(type);
+    }
+});
+
+test('Should get a sound by id', async () => {
+    const id = '701843824305710865';
+    const sound = await getSound(id);
+    expect(sound).toBeDefined()
+    expect(sound.id).toBe(id);
+});
---
+++ data/db/SoundDB.ts
@@ -0,0 +1,43 @@
+import { runQuery } from './db';
+export interface Sound {
+    id: string;
+	name?: string;
+	description?: string;
+	length?: number;
+	credit?: string;
+	url?: string;
+	type?: 'song' | 'sound' | 'guide';
+}
+
+function mapSound(row: any): Sound {
+    return {
+		id: row.id,
+		name: row.room_name,
+		description: row.description,
+		length: row.length ? parseInt(`${row.length}`) : null,
+		credit: row.credit,
+        url: row.url,
+		type: row.type,
+	};
+}
+
+export async function getSounds(): Promise<Array<Sound>> {
+	const query = `SELECT * FROM sound;`;
+	let { rows } = await runQuery(query);
+	return rows.map(mapSound);
+}
+
+export async function getSound(id: string): Promise<Sound> {
+    const query = `SELECT * FROM sound WHERE id = $1;`;
+	let { rows } = await runQuery(query, [id]);
+    if(rows.length) {
+        return rows.map(mapSound)[0];
+    }
+    return null;
+}
+
+export async function getSoundByType(type: string): Promise<Array<Sound>> {
+	const query = `SELECT * FROM sound WHERE "type" = $1;`;
+	let { rows } = await runQuery(query, [type]);
+	return rows.map(mapSound);
+}
