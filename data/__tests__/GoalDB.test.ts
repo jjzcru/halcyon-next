d21d617d38200db3dee6c3df9a3dc9c155c163ee
Jose J. Cruz <jjzcru@gmail.com>
2021-10-26 18:39:01 -0400

    feat: :sparkles: Add support for get, add and delete goal

--- data/__tests__/GoalDB.test.ts
+++ data/__tests__/GoalDB.test.ts
@@ -1,10 +1,78 @@
-import { getGoals } from '../db/GoalDB'
+import { Category } from '../db/EventDB';
+import { addGoal, getGoals, getGoal, updateGoal, deleteGoal, Frequency } from '../db/GoalDB'
 
-test.only('Should get the reminders by an employee id', async () => {
+test('Should get the reminders by an employee id', async () => {
     const employeeId = '698927090631649041';
     const goals = await getGoals(employeeId);
+
     expect(typeof goals).toBe('object');
     for(const goal of goals) {
         expect(goal.employeeId).toBe(employeeId);
     }
+});
+
+test('Should return a null goal because the id does not exist', async () => {
+    const id = '0';
+    const employeeId = '698927090631649041';
+    const goal = await getGoal(id, employeeId);
+
+    expect(goal).toBeNull();
+});
+
+test('Should return a goal by its id', async () => {
+    const id = '705217808479322113';
+    const employeeId = '698927090631649041';
+    const goal = await getGoal(id, employeeId);
+
+    expect(typeof goal).toBe('object');
+    expect(goal.id).toBe(id);
+    expect(goal.employeeId).toBe(employeeId);
+});
+
+test.skip('Should create a goal', async () => {
+    const target = 10;
+    const category: Category = 'break';
+    const frequency: Frequency = 'monthly';
+    const employeeId = '698927090631649041';
+    const goal = await addGoal(employeeId, target, category, frequency);
+
+    expect(typeof goal).toBe('object');
+    
+    expect(goal.employeeId).toBe(employeeId);
+    expect(goal.category).toBe(category);
+    expect(goal.frequency).toBe(frequency);
+});
+
+test.skip('Should update a goal', async () => {
+    let target = 10;
+    const category: Category = 'break';
+    let frequency: Frequency = 'monthly';
+    const employeeId = '698927090631649041';
+    let goal = await addGoal(employeeId, target, category, frequency);
+
+    expect(typeof goal).toBe('object');
+    expect(goal.employeeId).toBe(employeeId);
+    expect(goal.category).toBe(category);
+    expect(goal.frequency).toBe(frequency);
+
+    target = 30;
+    frequency = 'daily';
+    goal = await updateGoal(goal.id, employeeId, target, frequency);
+    expect(goal.category).toBe(category);
+    expect(goal.frequency).toBe(frequency);
+});
+
+test.only('Should delete the goal', async () => {
+    const target = 10;
+    const category: Category = 'break';
+    const frequency: Frequency = 'monthly';
+    const employeeId = '698927090631649041';
+    let goal = await addGoal(employeeId, target, category, frequency);
+
+    expect(typeof goal).toBe('object');
+    expect(goal.employeeId).toBe(employeeId);
+    expect(goal.category).toBe(category);
+    expect(goal.frequency).toBe(frequency);
+    const response = await deleteGoal(goal.id, employeeId);
+    expect(goal.id).toBe(response);
 });
--- data/db/GoalDB.ts
+++ data/db/GoalDB.ts
@@ -1,11 +1,14 @@
 import { runQuery } from './db';
+import { Category } from './EventDB';
+
+export type Frequency = 'daily' | 'weekly' | 'monthly' | 'yearly';
 
 export interface Goal {
 	id: string;
 	employeeId?: string;
 	target?: number;
-	category?: 'guided_meditation' | 'meditation' | 'water' | 'break';
-	frequency?: 'daily' | 'weekly' | 'monthly' | 'yearly';
+	category?: Category;
+	frequency?: Frequency;
 	createdAt?: Date;
 }
 
@@ -25,4 +28,57 @@ export async function getGoals(employeeId: string): Promise<Array<Goal>> {
 	const query = `SELECT * FROM goal WHERE employee_id = $1;`;
 	let { rows } = await runQuery(query, [employeeId]);
 	return rows.map(mapData);
+}
+
+export async function getGoal(id: string, employeeId: string): Promise<Goal> {
+	const query = `SELECT * FROM goal WHERE id = $1 AND employee_id = $2;`;
+	let { rows } = await runQuery(query, [id, employeeId]);
+	return rows.length ? rows.map(mapData)[0] : null;
+}
+
+export async function addGoal(
+	employeeId: string,
+	target: number,
+	category: Category,
+	frequency: Frequency
+): Promise<Goal> {
+	const query = `INSERT INTO goal (employee_id, target, category, frequency) 
+	VALUES ($1, $2, $3, $4) RETURNING id;`;
+	let { rows } = await runQuery(query, [
+		employeeId,
+		target,
+		category,
+		frequency,
+	]);
+	const id = rows[0].id;
+	return await getGoal(id, employeeId);
+}
+
+export async function updateGoal(
+    id: string,
+	employeeId: string,
+	target: number,
+	frequency: Frequency
+): Promise<Goal> {
+	const query = `UPDATE goal SET target = $1, frequency = $2
+	WHERE employee_id = $3 and id = $4 RETURNING id;`;
+	let { rows } = await runQuery(query, [
+		target,
+		frequency,
+		employeeId,
+        id
+	]);
+    return rows.length ? await getGoal(id, employeeId) : null;
+}
+
+export async function deleteGoal(
+    id: string,
+	employeeId: string,
+): Promise<string> {
+	const query = `DELETE FROM goal where id = $1 and employee_id = $2 returning id;`;
+	let { rows } = await runQuery(query, [
+        id,
+        employeeId
+	]);
+    return rows.length ? rows[0].id : null;
 }
