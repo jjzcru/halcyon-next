aab97fa7b392f0cc0aa5ea2be2f08ca640788023
Jose J. Cruz <jjzcru@gmail.com>
2021-11-20 22:16:46 -0500

    Add ngrok url

--- middleware/webauthn.js
+++ middleware/webauthn.js
@@ -1,7 +1,8 @@
 export const origin = [
     'http://localhost:3000',
     'http://localhost:5050',
-    'https://3783-69-120-130-235.ngrok.io',
     'https://halcyon-next-develop.vercel.app',
-    'https://halcyon-next.vercel.app'
+    'https://halcyon-next.vercel.app',
+    'https://3783-69-120-130-235.ngrok.io',
+    'https://d22c-69-120-130-235.ngrok.io'
 ];
